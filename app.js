faf7e45b89a035f4bdcf70057f7bb64c75ea01b8
Radek <Radeksat@gmail.com>
2020-04-30 15:56:59 +0200

    init

---
+++ app.js
@@ -0,0 +1,42 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');// doponierania sciezek a w tym przyapdku do poprania puvblic assety?
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');// loger z biblioteki morgan słuzy do zrzucnaia logów w trybie developerskim
+
+var indexRouter = require('./routes/index');// ta sa imorty podstawowoch storn głownej i uzytkownika
+var usersRouter = require('./routes/users');
+
+var app = express();// uruchamia nasz server
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// tutaj jest wywoaływane to co wczensiej zaimportowalem
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+// kiedy ktos bedzie chiał wejsc na jakas stornie niezadeklarowana!!
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
